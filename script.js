809128d9537b51a104e51e6ca8a243b54d6adf69
Kevin Weng <kevinweng70@gmail.com>
2024-06-15 17:16:17 -0400

    finished add and delete function

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
-
-
-
 let add = document.querySelector('.add'); // selects the add button
 add.onclick = function() { // turn it into a on-click function
     let ol = document.querySelector('ol'); // selects the ordered list tag
     let li = document.createElement('li'); // creates a <li> tag
     ol.append(li); // adds the new li tag into the ordered list
     li.innerText = "French Fries" // assigns the new <li> tag with the string "French Fries"
+}
+
+let deleteItem = document.querySelector('.delete'); // selects the delete button
+deleteItem.onclick = function() { // turn it into a on-click function
+    let ol = document.querySelector('ol'); // selects the ordered list tag
+    let li = document.querySelector('li:last-child'); // finds the last child of <li> and stores it into li
+    ol.removeChild(li); // removes the li child element from the ol
+    
 }
 
